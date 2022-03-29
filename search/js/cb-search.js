e32311bef0f3233c5dedba222918d7a5316877f4
Only <1065986093@qq.com>
2022-03-29 23:18:34 +0800

    完善搜索相关功能

--- search/js/cb-search.js
+++ search/js/cb-search.js
@@ -47,41 +47,43 @@ $(document).ready(function () {
         }
     });
 
-    // $("#cb-close-btn").click(function () {
-    //     $(".cb-search-tool").css("display", "none");
-    //     show = false;
-    //     time1 = 0;
-    // });
 
-    // $("#cb-search-btn").click(function () {
-    //     $(".cb-search-tool").css("display", "block");
-    //     show = true;
-    //     $("#cb-search-content").val("");
-    //     $("#cb-search-content").focus();
-    //     time1 = 0;
-    // });
 
-    // $.getJSON("/search/cb-search.json").done(function (data) {
-    //     if (data.code == 0) {
-    //         for (var index in data.data) {
-    //             var item = data.data[index];
-    //             names.push(item.title);
-    //             urls.push(item.url);
-    //         }
-    //
-    //         $("#cb-search-content").typeahead({
-    //             source: names,
-    //
-    //             afterSelect: function (item) {
-    //                 $(".cb-search-tool").css("display", "none");
-    //                 show = false;
-    //                 window.location.href = (urls[names.indexOf(item)]);
-    //                 return item;
-    //             }
-    //         });
-    //     }
-    // }).error(function (data, b) {
-    //     console.log("json解析错误，搜索功能暂不可用，请检查文章title，确保不含有换行等特殊符号");
-    // });
+    $("#cb-close-btn").click(function () {
+        $(".cb-search-tool").css("display", "none");
+        show = false;
+        time1 = 0;
+    });
+
+    $(".cb-search-btn").click(function () {
+        $(".cb-search-tool").css("display", "block");
+        show = true;
+        $("#cb-search-content").val("");
+        $("#cb-search-content").focus();
+        time1 = 0;
+    });
+
+    $.getJSON("/search/cb-search.json").done(function (data) {
+        if (data.code == 0) {
+            for (var index in data.data) {
+                var item = data.data[index];
+                names.push(item.title);
+                urls.push(item.url);
+            }
+
+            $("#cb-search-content").typeahead({
+                source: names,
+
+                afterSelect: function (item) {
+                    $(".cb-search-tool").css("display", "none");
+                    show = false;
+                    window.location.href = (urls[names.indexOf(item)]);
+                    return item;
+                }
+            });
+        }
+    }).error(function (data, b) {
+        console.log("json解析错误，搜索功能暂不可用，请检查文章title，确保不含有换行等特殊符号");
+    });
 
 });
